3f27465a36684486c2a3bcaf94742cfd6d3cc545
Umar7447 <muhammadumarprince7426@gmail.com>
2025-03-12 21:12:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Support System",
+  botOwner: 'Sheikh Umar', // Your name
+  ownerNumber: '923457508532', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
